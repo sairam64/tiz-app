880c9fe4279d6abba1d2bc81bfaecded6fd5db53
Prasoon Singh <prasoonsingh16@gmail.com>
2015-03-17 21:12:40 +0530

    Made table for vehicle list

--- js/car_input.js
+++ js/car_input.js
@@ -6,5 +6,16 @@ $(document).ready(function(){
 		name = $("#mdl_name").val();
 		year = $("#mdl_year").val();
 		console.log(name, year);
+		
+		var db = new Dexie("veh_db");
+		db.version(1).stores({ vehicle: "++Vid,v_name,v_year" });
+		db.open();
+		db.vehicle.add({v_name: name, v_year: year});
+		
+		db.transaction("rw", "vehicle", function(vehicle) {
+			vehicle.each(function(vehicle){
+				console.log(vehicle.v_name, vehicle.v_year);
+			});
+		});
 	});
 });
